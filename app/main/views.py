42687d1039ae803c5298d818fb1652862c59777c
Carol Wanjohi <wanjohi.carol54@gmail.com>
2019-09-14 12:38:57 +0300

    get bugs depending on the state and group them

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..requests import get_projects, get_unscheduled_bug_cards
-from ..utils import map_ids, map_names, filter_unstarted_bugs
+from ..utils import map_key_values, filter_bugs_states
 
 # Views
 @main.route('/')
@@ -15,10 +15,20 @@ def index():
     
     projects_found = get_projects()
     
-    project_ids = map_ids(projects_found)
+    project_ids = map_key_values(projects_found, 'id')
     
     bugs_found = get_unscheduled_bug_cards(project_ids)
 
-    unstarted_bugs = filter_unstarted_bugs(bugs_found)
+    unstarted_bugs_states = ['unscheduled', 'unstarted', 'planned', 'rejected']
+
+    unstarted_bugs = filter_bugs_states(bugs_found, unstarted_bugs_states)
+
+    inporgess_bugs_states = ['started', 'finished']
+
+    inporgess_bugs = filter_bugs_states(bugs_found, inporgess_bugs_states)
+
+    done_bugs_states = ['delivered', 'accepted']
+
+    done_bugs = filter_bugs_states(bugs_found, done_bugs_states)
     
-    return render_template('index.html', title = title, projects = unstarted_bugs )
+    return render_template('index.html', title = title, projects = done_bugs )
