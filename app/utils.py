656b05c1695c5bf5067ac4cd2ebfae79960b8ea3
Carol Wanjohi <wanjohi.carol54@gmail.com>
2019-09-14 12:40:26 +0300

    * refactor functions with similar behaviour

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,6 @@
 # TODO: Refactor
-def map_ids(list_of_dictionary):
-    return [project['id'] for project in list_of_dictionary if 'id' in project]
+def map_key_values(list_of_dictionary, key_wanted):
+    return [project[key_wanted] for project in list_of_dictionary if key_wanted in project]
 
-def map_names(list_of_dictionary):
-    return [project['name'] for project in list_of_dictionary if 'name' in project]
-
-def filter_unstarted_bugs(list_of_dictionary):
-    unstarted_bugs = ['unscheduled', 'unstarted', 'planned', 'rejected']
-    return [bug for bug in list_of_dictionary if bug['current_state'] in unstarted_bugs]
+def filter_bugs_states(list_of_dictionary, bugs_state):
+    return [bug for bug in list_of_dictionary if bug['current_state'] in bugs_state]
