154d66c52a4ca1e72fb0660a693320e5a22537b8
Carol Wanjohi <wanjohi.carol54@gmail.com>
2019-09-26 16:37:39 +0300

    add logic for posting to slack channel

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-from requests import get
+from requests import get, post
 
 # Getting the api key
 api_key = None
@@ -33,3 +33,15 @@ def get_unscheduled_bug_cards(project_ids):
         combined_list.extend(response.json())
         len(combined_list)
     return combined_list
+
+def post_to_bug_tracker_channel(list_of_bugs, state_of_bugs):
+    slack_acctachments = { "text": state_of_bugs, "attachments": list_of_bugs}
+    payload = json.dumps(slack_acctachments, default=obj_dict)
+    post(webhook, data=payload)
+    return "send to slack"
+
+def obj_dict(obj):
+    '''
+    Map object to a python dictionary
+    '''
+    return obj.__dict__
